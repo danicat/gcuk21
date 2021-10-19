efd1663e88c8d85175756f8f0feba49b2fa38458
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2021-10-19 22:57:12 +0100

    add step 01

---
+++ 01_helloworld/go.mod
@@ -0,0 +1,5 @@
+module github.com/danicat/gcuk21/01_helloworld
+
+go 1.16
+
+require github.com/hajimehoshi/ebiten/v2 v2.2.1
---
+++ 01_helloworld/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {}
+
+func (g *Game) Layout(width, height int) (int, int) {
+	return width, height
+}
+
+func main() {
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
