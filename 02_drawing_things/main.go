c8efa52f4a270c731b03ad365bd376077e49b254
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2021-10-25 07:22:10 +0100

    improve step 02

--- 02_drawing_things/main.go
+++ 02_drawing_things/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	_ "image/png"
 
@@ -10,23 +11,25 @@ import (
 )
 
 type Game struct {
-	img *ebiten.Image
+	img  *ebiten.Image
+	once sync.Once
 }
 
 func (g *Game) Update() error {
-	if g.img == nil {
+	g.once.Do(func() {
 		img, _, err := ebitenutil.NewImageFromFile("01_orientation.png")
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
 		g.img = img
-	}
+	})
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	op := ebiten.DrawImageOptions{}
 	op.GeoM.Scale(.2, .2)
+	op.GeoM.Translate(300, 200)
 	screen.DrawImage(g.img, &op)
 }
 
